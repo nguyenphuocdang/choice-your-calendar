e6e8f4e39c10ceea2b4461b252d4cb4166ff3601
DESKTOP-8A15AB0\UNIT <minhdangb3@gmail.com>
2023-06-02 21:22:29 +0700

    [DangNM] fix builder

--- build docker.bat
+++ build docker.bat
@@ -4,4 +4,4 @@ cd G:\HCMUTE\TLCN\temp fe\temp-choice-your-calendar
 
 docker build -t timechoice2 .
 
-docker run -p 4200:80 -p 443:443 --name timechoice timechoice2:latest
+docker run -p 80:80 -p 443:443 --name timechoice timechoice2:latest
