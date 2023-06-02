ef3095d91d369dd5a3142eb2980ef87923c82066
anakin9472 <nguyenphuocdang1234@gmail.com>
2023-06-03 05:38:28 +0700

    Update Public Event Detail
    Update Public Event Detail

--- src/app/components/organization/events/internal-events/public-event-details/public-event-details/public-event-details.component.ts
+++ src/app/components/organization/events/internal-events/public-event-details/public-event-details/public-event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SingleEventDetail } from 'src/app/_models/event';
 import { EventService } from 'src/app/_services/event.service';
 
 @Component({
@@ -11,16 +12,35 @@ export class PublicEventDetailsComponent implements OnInit {
   partnerEmail: string = '';
   partnerPathMapping: string = '';
   shareCode: string = '';
+  eventId: number = 0;
+  cexternalFlag: boolean = false;
+  eventInformation: SingleEventDetail = {
+    id: 0,
+    eventName: '',
+    eventDescription: '',
+    appointmentUrl: '',
+    eventStatus: '',
+    startTime: '',
+    endTime: '',
+    sendEmailFlag: false,
+    numberOfParticipants: 0,
+    publicModeFlag: false,
+    eventHosterId: 0,
+    date: '',
+  };
+  participantFlag: boolean = false;
+  emails: string[] = [];
   constructor(
     private Activatedroute: ActivatedRoute,
-    private eventService: EventService
+    private eventService: EventService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.Activatedroute.queryParamMap.subscribe((params) => {
       this.partnerPathMapping = params.get('partnerPathMapping') ?? '';
       this.shareCode = params.get('shareCode') ?? '';
-      debugger;
+      this._getPublicEventDetail();
     });
   }
 
@@ -34,6 +54,7 @@ export class PublicEventDetailsComponent implements OnInit {
         .subscribe((response: any) => {
           if (response.statusCode === 200) {
             debugger;
+            this.eventInformation = new SingleEventDetail(response.data);
           } else {
             debugger;
           }
@@ -41,5 +62,12 @@ export class PublicEventDetailsComponent implements OnInit {
     } catch (error) {
       debugger;
     }
+  }
+
+  onRedirectReschedule() {
+    const queryParams = this.Activatedroute.snapshot.queryParams;
+    this.router.navigate(['/public-event/reschedule-event'], {
+      queryParams,
+    });
   }
 }
